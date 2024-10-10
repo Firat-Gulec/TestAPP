eaa83a94debbaf79779c1ff6de1a8d34773729ad
Firat-Gulec <firat_gulec@hotmail.com>
2024-10-10 20:38:34 +0300

    Added Usercacheitem

--- Welcome/Welcome/Utils/Cache/UserCacheItems.swift
+++ Welcome/Welcome/Utils/Cache/UserCacheItems.swift
@@ -7,9 +7,41 @@
 
 import Foundation
 
-enum UserCacheItems {
-    case onboard
-    func read() -> Bool {
-        return false
+enum UserCacheItems: String {
+    case onboard = "onboard"
+}
+
+protocol UserCacheProtocol {
+    func read(key: UserCacheItems) -> String
+    func save(key: UserCacheItems, value: String)
+    func remove(key: UserCacheItems)
+}
+
+extension UserCacheProtocol {
+    /// Remove user data
+    /// - Parameter key: cache item key
+    func remove(key: UserCacheItems) {
+        UserDefaults.standard.removeObject(forKey: key.rawValue)
     }
+    
+    /// Read user data
+    /// - Parameter key: cache item key
+    /// - Returns: cache value or empty
+    func read(key: UserCacheItems) -> String {
+        // If user doesn't use userdefault it won't work. you should use coredata etc.
+        guard let value = UserDefaults.standard.string(forKey: key.rawValue) else { return "" }
+        return value
+    }
+    
+    /// Save user data
+    /// - Parameters:
+    ///   - key: cache item key
+    ///   - value: whatever user value as a string
+    func save(key: UserCacheItems, value: String) {
+        UserDefaults.standard.setValue(value, forKey: key.rawValue)
+    }
+}
+
+struct UserdefaultItems: UserCacheProtocol {
+    
 }
