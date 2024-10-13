329c9400f187d063d34c25cd9e97fab3ee1a00be
Firat-Gulec <firat_gulec@hotmail.com>
2024-10-13 10:04:32 +0300

    Added networkmanager

---
+++ Welcome/Welcome/Core/Network/NetworkManager.swift
@@ -0,0 +1,46 @@
+//
+//  NetworkManager.swift
+//  Welcome
+//
+//  Created by FiratGulec on 13.10.2024.
+//
+
+import Foundation
+import Alamofire
+
+struct NetworkConfiguration {
+    let baseURL: URL
+}
+
+protocol INetworkManager {
+    func fetch<T: Codable>(path: NetworkPath, method: HTTPMethod, type: T.Type) async -> T?
+    var configration: NetworkConfiguration { get set }
+}
+
+class NetworkManager: INetworkManager {
+    internal var configration: NetworkConfiguration
+    
+    init(configuration: NetworkConfiguration) {
+        self.configration = configuration
+    }
+    
+    func fetch<T: Codable>(path: NetworkPath, method: HTTPMethod, type: T.Type) async -> T? {
+        let dataRequest = AF.request("", method: method)
+            .validate()
+            .serializingDecodable(T.self)
+        
+        let result = await dataRequest.response
+        guard let value = result.value else {
+            print("ERROR : \\(String(discribing: result.error))")
+            return nil
+        }
+        
+        return value
+    }
+}
+
+enum NetworkPath: String {
+    case users = "api/users"
+    
+    static let baseURL: URL = .init(string: "https://reqres.ini")!
+}
